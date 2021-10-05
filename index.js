c7297f95f880118e07ac75e3c67f9f175b47b626
fdejesuscorrea <fdejesus.correa@udea.edu.co>
2021-10-05 13:09:10 -0500

    correxion de error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-const {PORT, CWD} = require("./config");
+const {PORT, ROOT} = require("./config");
 const {SignRouter,CarsRouter,CarUpRouter} =require("./routes");
 require("./data/db");
-server.use("/upload",express.static(CWD+"/upload"));
+server.use("/upload",express.static(ROOT+"/upload"));
 server.use(cors());
 server.use(express.json());
 server.use("/api",SignRouter);
